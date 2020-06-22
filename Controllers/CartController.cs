61d4d383cc9d13eea0cd14df0c46587b0a56938e
Việt Nguyễn <viet.nguyen.2000@hcmut.edu.vn>
2020-06-22 08:15:52 +0700

    Add script for cart page

--- Controllers/CartController.cs
+++ Controllers/CartController.cs
@@ -25,5 +25,62 @@ namespace bkfc.Controllers
             ViewData["cart"] = Cart.cart;
             return View();
         }
+        public int increase(int foodId)
+        {
+            for (int i =0 ; i<Cart.cart.Count ; i++)
+            {
+                if (foodId == Cart.cart[i].food.Id)
+                {
+                    Cart.cart[i].quantity++;
+                    ViewData["cart"]=Cart.cart;
+                    return Cart.cart[i].quantity;
+                }
+            }
+            return -1;
+        }
+        public int decrease(int foodId)
+        {
+            for (int i =0 ; i<Cart.cart.Count ; i++)
+            {
+                if (foodId == Cart.cart[i].food.Id)
+                {
+                    if(Cart.cart[i].quantity == 1)
+                    {
+                        remove(foodId);
+                        return 0;
+                    }
+                    else
+                    {
+                        Cart.cart[i].quantity-- ;
+                        ViewData["cart"]=Cart.cart;
+                        return Cart.cart[i].quantity;
+                    }
+                    
+                }
+            }
+            return -1;
+        }
+        public void change(int foodId, int quantity)
+        {
+            for (int i =0 ; i<Cart.cart.Count ; i++)
+            {
+                if (foodId == Cart.cart[i].food.Id)
+                {
+                    Cart.cart[i].quantity = quantity;
+                    return;
+                }
+            }
+        }
+        public void remove(int foodId)
+        {
+            for (int i =0 ; i<Cart.cart.Count ; i++)
+            {
+                if (foodId == Cart.cart[i].food.Id)
+                {
+                    Cart.cart.Remove(Cart.cart[i]);
+                    return;
+                }
+            }
+        }
     }
 }
