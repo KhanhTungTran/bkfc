3ec3801bcc57ed1107815d54bcc65931d43541ea
vietnguyen2000 <viet.nguyen.2000@hcmut.edu.vn>
2020-06-26 16:10:09 +0700

    Format price and fix search bar

--- wwwroot/js/upload.js
+++ wwwroot/js/upload.js
@@ -3,7 +3,7 @@ var feedback = function(res) {
         var get_link = res.data.link.replace(/^http:\/\//i, 'https://');
         document.querySelector('.status').classList.add('bg-success');
         document.querySelector('.status').innerHTML =
-            '<img class="img" alt="Imgur-Upload" src=\"' + get_link + '\"/>';
+            '<img class="img-up" alt="Imgur-Upload" src=\"' + get_link + '\"/>';
         document.getElementById("upload-img").value=get_link;
         document.getElementById("upload-img").readOnly=true;
 
