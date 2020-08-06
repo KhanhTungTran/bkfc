1dafe2f332805c99f874a2bc72c94807c110caf4
TungKhanhTran <tung.tran.1805@hcmut.edu.vn>
2020-08-06 23:55:12 +0700

    Update firebase version

--- Controllers/PaymentController.cs
+++ Controllers/PaymentController.cs
@@ -57,10 +57,10 @@ namespace bkfc.Controllers
             string accessKey = "0kA4tysnxO9tUKOx";
             string serectkey = "t2xCHeQr7pbnLBHqtutz5H1bt0hinODy";
             string orderInfo = "Đơn hàng từ Bách Khoa Food Court";
-            string returnUrl = "https://bkfc.azurewebsites.net/payment/done";
-            string notifyurl = "https://bkfc.azurewebsites.net/order";
-            //string returnUrl = "https://localhost:5001/payment/done";
-            //string notifyurl = "https://localhost:5001/order";
+            // string returnUrl = "https://bkfc.azurewebsites.net/payment/done";
+            // string notifyurl = "https://bkfc.azurewebsites.net/order";
+            string returnUrl = "https://localhost:5001/payment/done";
+            string notifyurl = "https://localhost:5001/order";
 
             string amount = money.ToString();
             string orderid = Guid.NewGuid().ToString();
